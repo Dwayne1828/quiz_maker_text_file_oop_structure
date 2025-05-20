08b1df690b0ed86686431345bccda95b22022e93
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-05-20 23:51:14 +0800

    working QuestionChecker class, changed method call

--- quiz_creator_text_file.py
+++ quiz_creator_text_file.py
@@ -1,35 +1,16 @@
 import os 
 from datetime import datetime
 from file_maker import FileMaker
-
-
-def question_checker(file_name):
-    #Check if the file exists, if not return 0
-    if not os.path.exists(file_name): 
-        return 1
-    
-    with open(file_name, "r") as quiz_file: 
-        lines = quiz_file.readlines()
-        for line in reversed(lines):
-            if line.startswith("Question"):
-                #Get the question number from the line
-                try:   
-                    question_number = int(line.split()[1].strip(":"))
-                    return question_number + 1
-                except (ValueError, IndexError):
-                    continue
-    
-    return 1 
+from question_checker import QuestionChecker
 
 #Function to asks the user for questions and answers and writes them to the file
 def create_quiz():
     #Get the file name from the user
-    making_file = FileMaker()
-    file_name = making_file.file_maker()
+    file_name = FileMaker().file_maker()
     
     #Starts the Question number at 1
-    question_number = question_checker(file_name)
-
+    question_number = QuestionChecker.question_checker(file_name)
+    
     #Open the file in append mode to create if exists or create a new one
     with open(file_name, "a") as quiz_file:
         #Get the current date and time
