0accadef2d5323783560cf4cf633b40e780efeb1
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-05-21 18:21:04 +0800

    new file for quiz_creator

---
+++ quiz_creator_files.py/quiz_creator_text_file.py
@@ -0,0 +1,6 @@
+from quiz_creator import QuizCreate
+
+if __name__ == "__main__":
+    quiz = QuizCreate()  # Create an instance of the QuizCreate class
+    quiz.quiz_content()  # Call the method to start creating the quiz
+
