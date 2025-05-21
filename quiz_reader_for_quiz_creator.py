c5c978547b5a9bf71c620a659bfdbe4a33fb63cb
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-05-21 23:34:49 +0800

    main file fixed redundant method calls

--- quiz_reader_for_quiz_creator.py
+++ quiz_reader_for_quiz_creator.py
@@ -5,17 +5,21 @@ from start_quiz import StartQuiz
 
 root = tk.Tk()
 root.withdraw()
-file_path = StartQuiz(root)
-file_path = file_path.start_quiz_with_file()
-QuizManage(root, file_path)
-root.title("Quiz")
-root.geometry("800x450")
-root.configure(bg="light blue")
-root.deiconify()
-root.mainloop()
 
 if __name__ == "__main__":
-    StartQuiz.start_quiz_with_file(root)
+    start_quiz = StartQuiz(root)
+    file_path = start_quiz.start_quiz_with_file()
+    if file_path:  
+        QuizManage(root, file_path)
+        root.title("Quiz")
+        root.geometry("800x450")
+        root.configure(bg="light blue")
+        root.deiconify()
+        root.mainloop()
+    else:
+        root.destroy()
+
+
 
 
 
